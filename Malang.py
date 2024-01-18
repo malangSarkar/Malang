6e37a982b28136487bbe906d5fdebbfbd2220ae7
malangSarkar <101513602+malangSarkar@users.noreply.github.com>
2024-01-19 00:04:36 +0500

    Update Malang.py

--- Malang.py
+++ Malang.py
@@ -1,10 +1,7 @@
-import os,platform
+import os
 os.system('git pull')
 print("Join Whatapss group")
 os.system('xdg-open https://chat.whatsapp.com/Fw5JNUdDbuPCtAHLrhLpt3')
 os.system("clear")
-xyz=platform.architecture()[0]
-if xyz=="32bit":
-    __import__("B2")
-elif xyz=="64bit":
-    __import__("B2")
+import mlng
+    
